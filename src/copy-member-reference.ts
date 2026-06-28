9688cf366acbf22223afa871ecac2d4e785210a0
Enrico <rico.bl@gmail.com>
2026-06-28 22:14:01 +1000

    Remove clipboardy in favour of native vscode clipboard

--- src/copy-member-reference.ts
+++ src/copy-member-reference.ts
@@ -1,16 +1,15 @@
 import * as vscode from 'vscode';
-import clipboard from 'clipboardy';
 
 import getMemberReference from './get-member-reference.js';
 
-export default function copyMemberReference() {
+export default async function copyMemberReference() {
   const memberReference = getMemberReference();
   if (!memberReference) {
     vscode.window.showInformationMessage('Failed to find member reference', {modal: true});
     return;
   }
 
-  clipboard.writeSync(memberReference);
+  await vscode.env.clipboard.writeText(memberReference);
 
   vscode.window.showInformationMessage(`${memberReference} copied to clipboard`, {modal: true});
 }
